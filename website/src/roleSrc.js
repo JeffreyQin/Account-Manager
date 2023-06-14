ed765fff5649c75bfbacc76927fcd26e6a18881a
JeffreyQin <122770444+JeffreyQin@users.noreply.github.com>
2023-06-14 15:13:45 -0400

    init commit

---
+++ website/src/roleSrc.js
@@ -0,0 +1,10 @@
+const clientButton = document.getElementById('clientButton');
+const moderatorButton = document.getElementById('moderatorButton')
+
+clientButton.addEventListener('click', () => {
+    window.location.href = './client/login.html';
+});
+
+moderatorButton.addEventListener('click', () => {
+    window.location.href = '.html';
+});
