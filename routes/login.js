be528e8558ebcb31599243434d0ae26838ea8338
JeffreyQin <122770444+JeffreyQin@users.noreply.github.com>
2023-06-16 15:22:26 -0400

    commit before middleware implementation

--- routes/login.js
+++ routes/login.js
@@ -7,8 +7,11 @@ require('dotenv').config();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(session({
-    secret: process.env.EXPRESS_SESSION_SECRET_KEY
-}))
+    secret: process.env.EXPRESS_SESSION_SECRET_KEY,
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 },
+    resave: false
+}));
 
 const connection = require('../connection.js');
 const config = require('../config.json');
@@ -17,11 +20,26 @@ const table = config.mysql.table;
 router.post('/username', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
-    checkCredentials('username', username, password, function(results) {
-        if (results == 0) {
+    checkCredentials('username', username, password, function(notFound) {
+        if (notFound) {
             res.send({ message: "Invalid credentials." })
         } else {
-
+            connection().query(`
+                SELECT *
+                FROM ${table}
+                WHERE username = '${username}';
+            `, (err, results) => {
+                if (err) {
+                    throw err;
+                } else {
+                    const profile = {
+                        email: results[0].email,
+                        username: results[0].username,
+                        password: results[0].password
+                    };
+                    
+                }
+            })
         }
     })
 });
@@ -34,6 +52,6 @@ function checkCredentials(option, user, pw, callback) {
         WHERE ${option} = '${user}' AND password = '${pw}';
     `, (err, results) => {
         if (err) throw err;
-        return callback(results.length);
+        return callback(results.length == 0);
     });
 }
--- website/src/client/loginSrc.js
+++ website/src/client/loginSrc.js
@@ -2,9 +2,10 @@ const usernameLoginButton = document.querySelector('#usernameLoginButton');
 const emailLoginButton = document.querySelector('#emailLoginButton');
 const userProfile = document.querySelector('#userProfile');
 const password = document.querySelector('#password');
+const loginMsg = document.querySelector('#loginMsg');
 
 usernameLoginButton.addEventListener('click', async () => {
-    const res = await fetch('http://localhost:3000/login/username', {
+    const result = await fetch('http://localhost:3000/login/username', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset=utf-8'
@@ -13,7 +14,12 @@ usernameLoginButton.addEventListener('click', async () => {
             username: userProfile.value,
             password: password.value
         })
-    });
+    }).then(res => res.json());
+    if (result.message == 'Invalid credentials.') {
+        loginMsg.innerHTML = result.message;
+    } else {
+        
+    }
 });
 
 emailLoginButton.addEventListener('click', () => {
